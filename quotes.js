1df32cb250e58e15ecfaa9d1f4c36073aeef9065
Caio Vaccaro <cavaccaro@hugeinc.com>
2015-05-11 11:49:10 -0300

    New lines

--- quotes.js
+++ quotes.js
@@ -1,45 +1,49 @@
 var colors = [
-			'#16a085',
-			'#2980b9',
-			'#34495e',
-			'#d35400',
-			'#7f8c8d',
-			'#c0392b'
-		];
+	'#16a085',
+	'#2980b9',
+	'#34495e',
+	'#d35400',
+	'#7f8c8d',
+	'#c0392b'
+];
 
-		var lines = [
-			'Novato tem que se fuder.',
-			'No final todo mundo vai me dar razão.',
-			'Almoçar com muita gente é uma merda. Nunca dá certo.',
-			'Essa juventude de hoje é muito mimada.',
-			'Tá achando que a vida é assim?',
-			'Cabô, cabô, cabô.',
-			'Você conhece um desenvolvedor?',
-			'A culpa é do desenvolvedor.',
-			'Eu to indo embora.',
-			'Não sei, se vira aí.',
-			'Que bagunça é essa aqui?',
-			'Só te digo o seguinte, nesse tempo que você ta fazendo isso aí, já tinha feito 5 telas.',
-			'Você sabe fazer um bot?',
-			'Tá se divertindo Alexis!?',
-			'Não, não, não, não, não. Não.',
-			'Eu tô preparado, sempre.',
-			'Que aula hein? Que aula.',
-			'Nossa, o Júlio é tão engraçado..',
-			'Faltar a uma pelada depois de confirmar, só em caso de extrema urgência.',
-			'Não é Marcos?',
-			'Que que é isso Marcola!?',
-			'Tem que tá focado.',
-			'Porra.',
-			'Ninguém mais tem compromisso com absolutamente nada!',
-			'Tá de folga?',
-			'Ta fazendo freela!?',
-			'O Salvador tem um humor muito particular.'
-		];
+var lines = [
+	'Novato tem que se fuder.',
+	'No final todo mundo vai me dar razão.',
+	'Almoçar com muita gente é uma merda. Nunca dá certo.',
+	'Essa juventude de hoje é muito mimada.',
+	'Tá achando que a vida é assim?',
+	'Cabô, cabô, cabô.',
+	'Você conhece um desenvolvedor?',
+	'A culpa é do desenvolvedor.',
+	'Eu to indo embora.',
+	'Não sei, se vira aí.',
+	'Que bagunça é essa aqui?',
+	'Só te digo o seguinte, nesse tempo que você ta fazendo isso aí, já tinha feito 5 telas.',
+	'Você sabe fazer um bot?',
+	'Tá se divertindo Alexis!?',
+	'Não, não, não, não, não. Não.',
+	'Eu tô preparado, sempre.',
+	'Que aula hein? Que aula.',
+	'Nossa, o Júlio é tão engraçado..',
+	'Faltar a uma pelada depois de confirmar, só em caso de extrema urgência.',
+	'Não é Marcos?',
+	'Que que é isso Marcola!?',
+	'Tem que tá focado.',
+	'Porra.',
+	'Ninguém mais tem compromisso com absolutamente nada!',
+	'Tá de folga?',
+	'Ta fazendo freela!?',
+	'O Salvador tem um humor muito particular.',
+	'Sorte? Sorte é para os fracos!',
+	'É muita vontade de vencer.',
+	'Que porra de calça é essa?',
+	'Essa calça no brasil é cartão amarelo.'
+];
 
-		function random(array) {
-			return array[Math.floor(Math.random() * array.length)];
-		}
+function random(array) {
+	return array[Math.floor(Math.random() * array.length)];
+}
 
-		document.getElementById('line').innerHTML = '"' + random(lines) + '"';
-		document.body.style.backgroundColor = random(colors);
+document.getElementById('line').innerHTML = '"' + random(lines) + '"';
+document.body.style.backgroundColor = random(colors);
